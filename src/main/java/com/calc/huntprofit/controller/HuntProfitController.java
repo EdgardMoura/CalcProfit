4621a4256bf39715b1aa6f1c37ccc0458f79d04a
EDGARD DE OLIVEIRA MOURA SANTOS <75489160+EdgardMoura@users.noreply.github.com>
2021-07-19 11:13:56 -0300

    Try delete 1

--- src/main/java/com/calc/huntprofit/controller/HuntProfitController.java
+++ src/main/java/com/calc/huntprofit/controller/HuntProfitController.java
@@ -1,6 +1,7 @@
 package com.calc.huntprofit.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,6 +21,12 @@ public class HuntProfitController {
 				+ "/imprimi/Elder Druid name/Ed spending/Party profit </b>";
 	}
 	
+	@RequestMapping(value = "/delete/")
+	public @ResponseBody String Informacao1() {
+				
+		return "<b>Non-existent id!! <br> ";
+	}
+	
 	@Autowired
 	HuntProfitRepository huntProfitRepository;
 	
@@ -33,10 +40,14 @@ public class HuntProfitController {
 
 	}
 	
+	/*@Autowired		
 	@RequestMapping(value = "/delete/{id}")
-	public @ResponseBody String Delete(@PathVariable Long id) {
+	public String Delete(@PathVariable Long id) {
+		
 		huntProfitRepository.deleteById(id);
 		
 		return "<b>Delete register done! <br> ";
 	}
+	*/
+	
 }
