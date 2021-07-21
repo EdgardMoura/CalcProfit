980eb22c362fdbfae9373f5112047d5bb8238b99
EDGARD DE OLIVEIRA MOURA SANTOS <75489160+EdgardMoura@users.noreply.github.com>
2021-07-20 22:50:43 -0300

    Find by part string

--- src/main/java/com/calc/huntprofit/repository/HuntProfitRepository.java
+++ src/main/java/com/calc/huntprofit/repository/HuntProfitRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.calc.huntprofit.util.HuntProfit;
@@ -19,13 +20,13 @@ public interface HuntProfitRepository extends JpaRepository<HuntProfit, Long>{
 	@Query(nativeQuery=true, value="select count(*) from public.hunt_profit where id = ?1")
     public int countItem(Long Id);
 	
-	@Query(nativeQuery=true, value="select count(*) from hunt_profit where nome_ed = ?1")
-    public String countItem(String NomeEd);
+	@Query(nativeQuery=true, value="select count(*) from hunt_profit where nome_ed like concat('%',:NomeEd,'%')")
+    public String countItem(@Param("NomeEd") String NomeEd);
 	
 	@Query(nativeQuery=true, value="select * from public.hunt_profit where id = ?1")
     public List<HuntProfit> findItem(Long Id);
 	
-	@Query(nativeQuery=true, value="select * from hunt_profit where nome_ed = ?1")
-    public <NomeEd> List<HuntProfit> findItem(String NomeEd);
+	@Query(nativeQuery=true, value="select * from hunt_profit where nome_ed like concat('%',:NomeEd,'%')")
+    public <NomeEd> List<HuntProfit> findItem(@Param("NomeEd") String NomeEd);
 
 }
